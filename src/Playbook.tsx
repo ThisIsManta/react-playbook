9924ba8670a40db26d4ad05d3f2be8a2bec37dad
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2020-10-19 17:37:53 +0700

    Add dark mode

--- src/Playbook.tsx
+++ src/Playbook.tsx
@@ -25,6 +25,11 @@ if (previewPathName) {
 	document.body.classList.add('playbook__preview')
 }
 
+const darkMode = window.localStorage.getItem('playbook__dark-mode') === 'true'
+if (darkMode) {
+	document.body.classList.add('playbook__dark-mode')
+}
+
 export default React.memo((props: Props) => {
 	const pages = useMemo(
 		() => _.chain(props.pages)
@@ -174,6 +179,23 @@ function Playbook(props: Props) {
 						? props.contentControl
 						: <props.contentControl />}
 					<div style={{ flex: '1 1 auto' }} />
+					<PlaybookButton
+						title='Toggle dark mode'
+						onClick={() => {
+							if (darkMode) {
+								window.localStorage.removeItem('playbook__dark-mode')
+							} else {
+								window.localStorage.setItem('playbook__dark-mode', 'true')
+							}
+
+							window.location.reload()
+						}}
+					>
+						{darkMode
+							? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none" /><path d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79 1.42-1.41zM4 10.5H1v2h3v-2zm9-9.95h-2V3.5h2V.55zm7.45 3.91l-1.41-1.41-1.79 1.79 1.41 1.41 1.79-1.79zm-3.21 13.7l1.79 1.8 1.41-1.41-1.8-1.79-1.4 1.4zM20 10.5v2h3v-2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm-1 16.95h2V19.5h-2v2.95zm-7.45-3.91l1.41 1.41 1.79-1.8-1.41-1.41-1.79 1.8z" /></svg>
+							: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none" /><path d="M10 2c-1.82 0-3.53.5-5 1.35C7.99 5.08 10 8.3 10 12s-2.01 6.92-5 8.65C6.47 21.5 8.18 22 10 22c5.52 0 10-4.48 10-10S15.52 2 10 2z" /></svg>
+						}
+					</PlaybookButton>
 					<PlaybookButton
 						id='playbook__property-panel-toggle'
 						title={propertyPanelVisible ? 'Hide property panel' : 'Show property panel'}
